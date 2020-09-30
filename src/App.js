fbd3696f2478ee5cc79a8e70ea0a0e6c711ca9fd
varakjan <varak.jan.dev@gmail.com>
2020-09-30 10:16:45 +0200

    Workshop #3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
-import ExpenseWrapper from './ExpenseWrapper'
+import Content from './Content'
+import DarkThemeContext from './DarkThemeContext'
+import Navbar from './Navbar'
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(true)
+  const contextValue = { darkTheme, setDarkTheme }
   return (
-    <div className="App">
-      <ExpenseWrapper />
-    </div>
+    <Router>
+      <DarkThemeContext.Provider value={contextValue}>
+        <div className="App">
+          <Navbar />
+          <Content />
+        </div>
+      </DarkThemeContext.Provider>
+    </Router>
   )
 }
 
--- src/Expense.jsx
+++ src/Expense.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useHistory } from 'react-router'
 import './Expense.css'
 
-
 export default function Expense({ expense, removeExpense }) {
+  const history = useHistory()
   return (
     <div className="expense">
       {`name: ${expense.name}, amount: ${expense.amount}`}
       <button onClick={() => removeExpense(expense.id)}>Remove</button>
+      <button onClick={() => history.push(`/expense/${expense.id}`)}>
+        Details
+      </button>
     </div>
   )
 }
